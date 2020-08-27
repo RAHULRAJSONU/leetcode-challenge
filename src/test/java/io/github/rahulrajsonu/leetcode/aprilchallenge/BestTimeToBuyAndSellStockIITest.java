a2ca5b56fc4dbf3d04118020c12b1fead0ae6e32
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-08-27 18:09:33 +0530

    test case added

--- src/test/java/io/github/rahulrajsonu/leetcode/aprilchallenge/BestTimeToBuyAndSellStockIITest.java
+++ src/test/java/io/github/rahulrajsonu/leetcode/aprilchallenge/BestTimeToBuyAndSellStockIITest.java
@@ -1,4 +1,15 @@
+package io.github.rahulrajsonu.leetcode.aprilchallenge;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BestTimeToBuyAndSellStockIITest {
-  
+
+    @Test
+    void maxProfit() {
+        assertEquals(7,BestTimeToBuyAndSellStockII.maxProfit(new int[]{7,1,5,3,6,4}));
+        assertEquals(4,BestTimeToBuyAndSellStockII.maxProfit(new int[]{1,2,3,4,5}));
+        assertEquals(0,BestTimeToBuyAndSellStockII.maxProfit(new int[]{7,6,4,3,1}));
+    }
 }
