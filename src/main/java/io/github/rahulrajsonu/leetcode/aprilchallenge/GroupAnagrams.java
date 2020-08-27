2e5fcd2d49da42fa29b6a60c0f230ae4d4ffa803
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-08-27 18:44:24 +0530

    Optimized solution added for groupAnagram problem

--- src/main/java/io/github/rahulrajsonu/leetcode/aprilchallenge/GroupAnagrams.java
+++ src/main/java/io/github/rahulrajsonu/leetcode/aprilchallenge/GroupAnagrams.java
@@ -1,16 +1,15 @@
 package io.github.rahulrajsonu.leetcode.aprilchallenge;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class GroupAnagrams {
 
   public static void main(String[] args) {
-    groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}).forEach(System.out::println);
+//    groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}).forEach(System.out::println);
+    groupAnagramOptimized(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}).forEach(System.out::println);
+//            .forEach((k,v)-> System.out.println(k+" :: "+v));
   }
 
   /**
@@ -47,5 +46,34 @@ public class GroupAnagrams {
       }
     }
     return new ArrayList<>(grouped.values());
+  }
+
+  /**
+   * Given an array of strings, group anagrams together.
+   *
+   * Example:
+   *
+   * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+   * Output:
+   * [
+   *   ["ate","eat","tea"],
+   *   ["nat","tan"],
+   *   ["bat"]
+   * ]
+   * Note:
+   *
+   * All inputs will be in lowercase.
+   * The order of your output does not matter.
+   * @param strs
+   * @return
+   */
+  public static Collection<List<String>> groupAnagramOptimized(String[] strs){
+    return Arrays.stream(strs)
+            .collect(
+                    Collectors.groupingBy(
+                            str->{
+                              String[] s = str.split("");
+                              Arrays.sort(s);
+                              return String.join("",s);})).values();
   }
 }
